bd871ee84a4510a54382839b62bb09a36fe444d1
George Lam <georgelamd@gmail.com>
2024-05-09 13:50:54 -0400

    Add database to Docker container

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM python:latest
-RUN apt-get update && apt-get install -y postgresql-client
+RUN apt-get update && \
+    apt-get install -y postgresql-client && \
+    pip install requests
 COPY /scripts/etl_script.py .
 CMD ["python", "etl_script.py"]
--- scripts/etl_script.py
+++ scripts/etl_script.py
@@ -1,28 +1,33 @@
 import requests
 
-API_ENDPOINT_ANIME = '/anime'
 API_URL = 'https://kitsu.io/api/edge'
 
 
-def get(url):
+def get_anime():
+    url = f'{API_URL}/anime'
     response = requests.get(url)
-    if response.status_code == 200 and 'data' in response.json():
+
+    num_gets = 0  # for testing
+
+    while response is not None and num_gets < 1:
+        if response.status_code != 200:
+            print(f'Error: Status code: {response.status_code}.')
+            exit(1)
+        elif 'data' not in response.json():
+            print('Error: There is no data in the response.')
+            exit(1)
+
         data = response.json()['data']
         for key in data:
             print(key)
+
         if 'links' in response.json():
             links = response.json()['links']
             if 'next' in links:
-                endpoint = links['next'].split
-                get(links['next'])
-    else:
-        print(f'Error: {response.status_code}')
-        exit(1)
+                url = links['next']
+                response = requests.get(url)
+
+        num_gets += 1
 
 
-headers = {
-    'Accept': 'application/vnd.api+json',
-    'Content-Type': 'application/vnd.api+json'
-}
-
-get(API_URL + API_ENDPOINT_ANIME)
+get_anime()
